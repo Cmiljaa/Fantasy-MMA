0350ef4a359d6518e4e092adb24bda0d5ef8c14e
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2026-03-16 23:20:57 +0100

    Frontend init

---
+++ Frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<div>
+		<h1 class="text-2xl cursor  font-bold underline bg-red-800">
+			Hello world!
+		</h1>
+		<h2 class="bg-red-500 cursor-pointer">
+			Hello world!
+		</h2>
+	</div>
+</template>
