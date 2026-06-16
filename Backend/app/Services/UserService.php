7c1916c15b75731b756846da255bf9d8372dc56b
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2026-06-16 23:51:12 +0200

    Add deleteUser method to UserService

--- Backend/app/Services/UserService.php
+++ Backend/app/Services/UserService.php
@@ -9,4 +9,17 @@ use Illuminate\Http\JsonResponse;
 
 class UserService {
 
+	public function deleteUser(Request $request, User $user): JsonResponse
+	{
+		Auth::logout();
+		$request->session()->invalidate();
+		$request->session()->regenerateToken();
+
+		$user->delete();
+
+		return response()->json([
+			"success" => true,
+			"message" => "User successfully deleted."
+		]);
+	}
 }
