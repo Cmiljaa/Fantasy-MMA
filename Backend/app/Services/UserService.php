0dcb04be66c57b1a170ae505dfe449fc975fa1bc
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2026-06-16 23:50:51 +0200

    Create user service

---
+++ Backend/app/Services/UserService.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+
+class UserService {
+
+}
