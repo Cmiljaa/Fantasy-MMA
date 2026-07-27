338b9921ea06d79b5eb138895fc47e8d7bba01db
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2026-07-27 23:37:08 +0200

    Replace App template with RouterView

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,13 +1,7 @@
-<script setup lang="ts">
-</script>
-
 <template>
-	<div>
-		<h1 class="text-2xl cursor  font-bold underline bg-red-800">
-			Hello world!
-		</h1>
-		<h2 class="bg-red-500 cursor-pointer">
-			Hello world!
-		</h2>
-	</div>
+	<RouterView></RouterView>
 </template>
+
+<script setup lang="ts">
+
+</script>
