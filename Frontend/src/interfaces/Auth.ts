8019b103162686099ccada674daf38243e4b15d2
Katušnik <147316652+Cmiljaa@users.noreply.github.com>
2026-08-01 23:43:16 +0200

    Add Auth interface

---
+++ Frontend/src/interfaces/Auth.ts
@@ -0,0 +1,11 @@
+export interface SignUpForm {
+	username: string
+	email: string
+	password: string
+	repeatPassword: string
+}
+
+export interface SignInForm {
+	email: string
+	password: string
+}
